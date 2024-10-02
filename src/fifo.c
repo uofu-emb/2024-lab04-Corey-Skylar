de1b79b365a0714ad5a19a9e922d5b5bb2862266
Corey Buchanan <coreybuchanan.engineer@gmail.com>
2024-10-02 12:49:15 -0600

    Complete fifo.

--- src/fifo.c
+++ src/fifo.c
@@ -1,5 +1,12 @@
 #include "fifo.h"
 
 void fifo_worker_handler(QueueHandle_t requests, QueueHandle_t results, int id) {
+    while (1) {
+        struct request_msg data = {};
 
+        xQueueReceive(requests, &data, portMAX_DELAY);
+        data.output = data.input + 5;
+        data.handled_by = id;
+        xQueueSendToBack(results, &data, portMAX_DELAY);
+    }
 }
