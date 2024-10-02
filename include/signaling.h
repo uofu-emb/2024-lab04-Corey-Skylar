2fc0dc58e3c1ad985b4648a6faeb414ceee4e2d4
Corey Buchanan <coreybuchanan.engineer@gmail.com>
2024-10-02 12:34:15 -0600

    Add signal implementation.

--- include/signaling.h
+++ include/signaling.h
@@ -7,10 +7,12 @@ struct signal_data {
     int32_t output;
 };
 
+// Worker
 void signal_handle_calculation(SemaphoreHandle_t request,
                                SemaphoreHandle_t response,
                                struct signal_data *data);
 
+// Request
 BaseType_t signal_request_calculate(SemaphoreHandle_t request,
                                     SemaphoreHandle_t response,
                                     struct signal_data *data);
